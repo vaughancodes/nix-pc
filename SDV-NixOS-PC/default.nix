0abc70b007f202b9435887001073315f7ae05bfc
Daniel Vaughan <127461407+vaughancodes@users.noreply.github.com>
2026-04-07 02:39:56 +0000

    Change hardware configuration path to temporary file

--- SDV-NixOS-PC/default.nix
+++ SDV-NixOS-PC/default.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /tmp/hardware-configuration.nix
       ./users-and-groups.nix
       ./system-configuration.nix
     ];
