10684212f7fedad50105daa79c84b9ef55f40529
Daniel Vaughan <127461407+vaughancodes@users.noreply.github.com>
2026-04-07 03:02:32 +0000

    Change hardware configuration file path

--- SDV-NixOS-PC/default.nix
+++ SDV-NixOS-PC/default.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      /tmp/hardware-configuration.nix
+      ./hardware-configuration.nix
       ./users-and-groups.nix
       ./system-configuration.nix
     ];
